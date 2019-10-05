9dfa5b064a1efa368fcc25f4bd56aebc83b419c9
Craig Gabel <gabe2453@stthomas.edu>
2019-10-05 16:13:51 -0500

    There were some strange errors with numerous classes.  Hopefully this fixes them...

--- SkunkProject/SkunkProject/src/SkunkApp.java
+++ SkunkProject/SkunkProject/src/SkunkApp.java
@@ -1,11 +1,14 @@
 import edu.princeton.cs.introcs.StdOut;
-   public class SkunkApp {
-		    public static void main(String[] args) {
-		    	
-		    	StdOut.println("Welcome to Skunk game!"); //to print a welcome massage.
-		    	Dice newDice = new Dice(); // to create a Dice instance.
-		    	newDice.roll(); // Invoke roll().
-		    	StdOut.println(newDice); 
-	      }
 
-	}
+public class SkunkApp
+{
+    public static void main(String[] args)
+    {
+		    	
+    	StdOut.println("Welcome to Skunk game!"); //to print a welcome massage.
+
+    	Dice newDice = new Dice();
+    	newDice.roll(); // Invoke roll().
+    	StdOut.println(newDice); 
+    }
+}
