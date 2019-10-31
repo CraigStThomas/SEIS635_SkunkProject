48688d332361259137a2abdfa9bb2b95d69179f9
Craig Gabel <gabe2453@stthomas.edu>
2019-10-30 23:10:12 -0500

    added/tweaked some unit tests for Game.java, removed SkunkAppTest.java

--- SkunkProject/SkunkProject/test/GameTest.java
+++ SkunkProject/SkunkProject/test/GameTest.java
@@ -35,5 +35,34 @@ public class GameTest
 		myGame.addPlayer("myPlayer1");
 		myGame.setCurrentPlayer(1);
 		assertEquals("Game.Player Error.", "myPlayer1", myGame.getCurrentPlayer().getName());
+		myGame.addPlayer("myPlayer2");
+		myGame.setCurrentPlayer(2);
+		assertEquals("Game.Player Error.", "myPlayer2", myGame.getCurrentPlayer().getName());
+	}
+	
+	@Test
+	public void testTest()
+	{
+		myGame.addPlayer("myPlayer");
+		myGame.addPlayer("myPlayer1");
+		myGame.addPlayer("myPlayer2");
+		myGame.addPlayer("myPlayer3");
+		myGame.setCurrentPlayer(3);
+		myGame.getCurrentPlayer().setChips(0);
+		myGame.initGameState();
+		myGame.setCurrentPlayer(0);
+		myGame.getCurrentPlayer().setPlayerScore(0);
+		myGame.setCurrentPlayer(1);
+		myGame.getCurrentPlayer().setPlayerScore(5);
+		myGame.setCurrentPlayer(2);
+		myGame.getCurrentPlayer().setPlayerScore(10);
+		myGame.identifyNextPlayer();
+		myGame.identifyNextPlayer();
+		myGame.identifyNextPlayer();
+		myGame.identifyNextPlayer();
+		myGame.identifyNextPlayer();
+		myGame.identifyNextPlayer();
+		myGame.identifyNextPlayer();
+		myGame.endGameChipDistribution(myGame.determineWinningPlayer());
 	}
 }
